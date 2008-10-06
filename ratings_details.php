c5ba3be15487ff7d238479e418151943934b8cf6
darkcanuck <jerome@darkcanuck.net>
2008-10-06 17:44:37 +0000

    Added stats block to detail display
    git-svn-id: http://darkcanuck.net/svn/rumbleserver/trunk@17 8339339b-bd71-e8d0-a093-ba98e431ba4e

--- ratings_details.php
+++ ratings_details.php
@@ -21,24 +21,8 @@ $allrows = $pairings->getBotPairings($gametype->getCode(), $bot['bot_id']);
 $partylist = $party->getList();
 $glicko = new GlickoRating();
 
-
-//output header
-echo "<h2>RATING DETAILS FOR $name IN GAME $game</h2>
-<h3>CURRENT SCORE = " . number_format($bot['score_pct']/1000, 3) . "</h3>
-<p>Participated in {$bot['battles']} battles since {$bot['created']}  Last battle held {$bot['timestamp']}.</p>
-<table border=1>
-<tr>
-	<td><b><a href='RatingsDetails?game=$game&name=$name&sort=vs_name' title='Sort by Name'>Enemy</a></b></td>
-	<td><b><a href='RatingsDetails?game=$game&name=$name&sort=score_pct' title='Sort by APS'>% Score</a></b></td>
-	<td><b><a href='RatingsDetails?game=$game&name=$name&sort=rating_glicko' title='Sort by Rating'>G-Rating</a></b></td>
-	<td><b><a href='RatingsDetails?game=$game&name=$name&sort=battles' title='Sort by Battles'>Battles</a></b></td>
-	<td><b><a href='RatingsDetails?game=$game&name=$name&sort=timestamp' title='Sort by Time'>Last Battle</a></b></td>
-	<td><b>Battles</b></td>
-	<td><b><a href='RatingsDetails?game=$game&name=$name&sort=expected' title='Sort by Expected %'>Expected %</a></b></td>
-	<td><b><a href='RatingsDetails?game=$game&name=$name&sort=pbindex' title='Sort by PBI'>ProblemBot Index</a></b></td>	
-</tr>";
-
 // calculate & sort
+$standarddev = 0.0;
 $momentum = 0.0;
 $specialization = 0.0;
 $fields = null;
@@ -53,6 +37,7 @@ foreach ($allrows as $k=>$rs) {
 									$partylist[ $rs['vs_id'] ]['rd_glicko']
 									);
 	$allrows[$k]['pbindex'] = $rs['score_pct']/1000.0 - $allrows[$k]['expected'];
+	$standarddev += pow(($allrows[$k]['score_pct'] - $bot['score_pct'])/100000.0, 2.0);
 	$momentum += 3.0 * $allrows[$k]['pbindex'];
 	$specialization += pow($allrows[$k]['pbindex']/100.0, 2.0);
 	
@@ -67,13 +52,51 @@ foreach ($allrows as $k=>$rs) {
 		$sortname[$k] = $allrows[$k]['vs_name'];
 	}
 }
+$standarddev = sqrt($standarddev / $bot['pairings']) * 100.0;
 if ($sort!=null)
 	array_multisort($sortcol, ($sort=='vs_name') ? SORT_ASC : SORT_DESC, $sortname, SORT_ASC, $allrows);
+
+//output header
+echo "<h2>RATING DETAILS FOR $name IN GAME $game</h2>
+<table border='1'>
+	<tr><th colspan='2'>Stats for $name</th></tr>
+	<tr><td>\"Classic\" Elo Rating</td><td>" . number_format($bot['rating_classic']/1000, 1, '.', '') . "</td></tr>
+	<tr><td>Glicko Rating (RD)</td><td>" . number_format($bot['rating_glicko']/1000, 1, '.', '') .
+	 									" (" . number_format($bot['rd_glicko']/1000, 0)  . ")</td></tr>
+	<tr><td>Glicko-2 (RD, volatility)</td><td>" . number_format($bot['rating_classic']/1000, 1, '.', '') 									.
+										 " (" . number_format($bot['rd_glicko2']/1000, 0)  . 
+										 ", " . number_format($bot['vol_glicko2']/1000000, 3)  . ")</td></tr>
+	<tr><td>Rating Momentum</td><td>" . number_format($momentum, 3) . "</td></tr>
+	<tr><td>Specialization</td><td>" . number_format($specialization*100.0, 3) . "</td></tr>
+	<tr><td>Average % Score (APS)</td><td>" . number_format($bot['score_pct']/1000, 3) . " %</td></tr>
+	<tr><td>Standard Deviation</td><td>" . number_format($standarddev, 3) . "</td></tr>
+	<tr><td>Average % Survival</td><td>" . number_format($bot['score_survival']/1000, 3) . " %</td></tr>
+	<tr><td># Battles</td><td>{$bot['battles']}</td></tr>
+	<tr><td># Pairings</td><td>{$bot['pairings']}</td></tr>
+	<tr><td># Pairs Won</td><td>{$bot['count_wins']} (" . number_format($bot['count_wins']/$bot['pairings'] * 100.0, 1) . "%)</td></tr>
+	<tr><td>Added to Rumble</td><td>{$bot['created']}</td></tr>
+	<tr><td>Last Battle</td><td>{$bot['timestamp']}</td></tr>
+</table>
+
+<table border=1>
+<tr>
+	<td><b><a href='RatingsDetails?game=$game&name=$name&sort=vs_name' title='Sort by Name'>Enemy</a></b></td>
+	<td><b><a href='RatingsDetails?game=$game&name=$name&sort=score_pct' title='Sort by APS'>% Score</a></b></td>
+	<td><b><a href='RatingsDetails?game=$game&name=$name&sort=rating_glicko' title='Sort by Rating'>G-Rating</a></b></td>
+	<td><b><a href='RatingsDetails?game=$game&name=$name&sort=battles' title='Sort by Battles'>Battles</a></b></td>
+	<td><b><a href='RatingsDetails?game=$game&name=$name&sort=timestamp' title='Sort by Time'>Last Battle</a></b></td>
+	<td><b>Battles</b></td>
+	<td><b><a href='RatingsDetails?game=$game&name=$name&sort=expected' title='Sort by Expected %'>Expected %</a></b></td>
+	<td><b><a href='RatingsDetails?game=$game&name=$name&sort=pbindex' title='Sort by PBI'>ProblemBot Index</a></b></td>	
+</tr>";
+
 
 // output data
 foreach ($allrows as $rs) {
 	$cell_colour = ($rs['score_pct']>60000) ? ' bgcolor="#99CC00"' : 
 				 ( ($rs['score_pct']<40000) ? ' bgcolor="#FF6600"' :  '');
+	$pbi_colour = ($rs['pbindex']>=10.0) ? ' bgcolor="#99CC00"' : 
+				 ( ($rs['pbindex']<=-10.0) ? ' bgcolor="#FF6600"' :  '');
 	echo "<tr>";
 	echo "<td>{$rs['vs_name']}</td>";
 	echo "<td" . $cell_colour . ">" . number_format($rs['score_pct']/1000, 3)  . "</td>";
@@ -85,12 +108,10 @@ foreach ($allrows as $rs) {
 				. "&name=" . htmlspecialchars($name)
 				. "&vs="   . htmlspecialchars($rs['vs_name']) . "'>battles</a></td>";
 	echo "<td>" . number_format($rs['expected'], 1) . "</td>";
-	echo "<td>" . number_format($rs['pbindex'], 1) . "</td>";
+	echo "<td" . $pbi_colour . ">" . number_format($rs['pbindex'], 1) . "</td>";
 	echo "</tr>\n";
 }
 
 //output footer
 echo "</table>\n";
-echo "<p><b>Momentum = " . number_format($momentum, 3) . "</b></p>\n";
-echo "<p><b>Specialization = " . number_format($specialization*100.0, 3) . "</b></p>\n";
 ?>

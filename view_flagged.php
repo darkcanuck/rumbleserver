b865840c9602b9dd1ae37d67652274c0ee594a1c
"Jerome Lavigne ext:(%22) <jerome@darkcanuck.net>
2011-11-13 14:39:39 -0500

    Removed SVN keyword expansions

--- view_flagged.php
+++ view_flagged.php
@@ -2,12 +2,7 @@
 /******************************************************************************
  * Flagged Battles View --  Darkcanuck's Roborumble Server
  *
- * $HeadURL$
- * $Date$
- * $Revision$
- * $Author$
- *
- * Copyright 2008-2009 Jerome Lavigne (jerome@darkcanuck.net)
+ * Copyright 2008-2011 Jerome Lavigne (jerome@darkcanuck.net)
  * Released under GPL version 3.0 http://www.gnu.org/licenses/gpl-3.0.html
  *****************************************************************************/
 
